8a4249e02143debdcc9350742717661ff7d55484
Phyth22 <113887340+Phyth22@users.noreply.github.com>
2023-09-14 14:26:21 +0300

    Add files via upload
    Assignment lists

---
+++ lists.py
@@ -0,0 +1,31 @@
+#change 80 to in the list.
+
+student_marks = [60,80,90,98]
+student_marks [1] = 89
+#print(student_marks)
+
+#add a new item 55(append a new list)
+student_marks.append(55)
+#print(student_marks)
+
+#finding the size of the item having appended 55
+#print(len(student_marks))
+
+#write a python program to sum all the items in the list
+student_marks = [60,80,90,98]
+import math
+#student_marks = math.fsum(60,80,90,98)
+#print(student_marks)
+
+#sum(list)
+sum(student_marks)
+#print(sum(student_marks))
+#write a python function that takes two lists and returns true, if they have atleast one common number
+x = input("Enter item:")
+y = input("Enter item:")
+print("True, there is a common member in x and y")
+   
+ 
+
+
+
